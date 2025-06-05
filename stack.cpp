eaff75f805f32e185cdc5e5906ae9b52c33c2341
Faid Arya Pamungkas <faid.arya.ft24@mail.umy.ac.id>
2025-06-05 15:28:24 +0700

    Membuat While

--- stack.cpp
+++ stack.cpp
@@ -70,4 +70,13 @@ int main(){
     int choice = 0;
     int value;
 
-    
+    while (choice != 4){
+        
+        cout <<"1. Push\n";
+        cout <<"2. Pop\n";
+        cout <<"3. Peek\n";
+        cout <<"4. Exit\n";
+        cout <<"Enter Your Choice: ";
+        cin >> choice;
+
+      
